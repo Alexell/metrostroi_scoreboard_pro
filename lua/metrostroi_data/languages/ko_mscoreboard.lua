5262693e5fa3238bb4e561977a07fe1a1fb9713d
Alexell <alexell@alexell.ru>
2019-11-13 18:40:22 +0300

    Добавлена Корейская локаль

---
+++ lua/metrostroi_data/languages/ko_mscoreboard.lua
@@ -0,0 +1,31 @@
+return [[
+[en]
+AuthorTextMScoreBoard = Archerity (STEAM_0:1:40909285)
+#Train entities
+Entities.gmod_subway_81-717_6.Name = 81-717.6
+Entities.gmod_subway_ezh3ru1.Name = 81-710 (Ezh3 RU1)
+#MScoreBoard
+MScoreBoard.CLTime = 현재 시간
+MScoreBoard.SVTime = 서버 시간
+MScoreBoard.Nick = 이름
+MScoreBoard.Rank = 등급
+MScoreBoard.Route = 열번
+MScoreBoard.Wagons = 량수
+MScoreBoard.Train = 차량
+MScoreBoard.Station = 역/진행방향
+MScoreBoard.Line = Line
+MScoreBoard.Hours = 시간
+MScoreBoard.Ping = 핑
+MScoreBoard.Players = 플레이어
+MScoreBoard.TransPass = %s명의 승객을 수송하였습니다.
+#ClientPanel
+MSCP.Colors = 색상
+MSCP.Header1 = 상단부 배경
+MSCP.Header2 = 상단부 글자
+MSCP.Frame1 = 플레이어 프레임 배경
+MSCP.Frame2 = 플레이어 프레임 글자
+MSCP.Frame3 = 플레이어 프레임 전경
+MSCP.Footer1 = 하단부 배경
+MSCP.Footer2 = 하단부 글자
+MSCP.Reset = 기본 색상으로 변경
+]]
