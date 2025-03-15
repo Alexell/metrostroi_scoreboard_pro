96491e17cb57eccfd0657d9fa02e61369bddcbbf
Tora <49911006+NovaDiablox@users.noreply.github.com>
2025-03-15 05:00:53 +0300

    Update Turkish

--- lua/metrostroi_data/languages/tr_mscoreboard.lua
+++ lua/metrostroi_data/languages/tr_mscoreboard.lua
@@ -1,16 +1,16 @@
 return [[
 [tr]
 
-AuthorTextMScoreBoard = Matt (STEAM_0:1:241718138)
+AuthorTextMScoreBoard = Tora (STEAM_0:1:241718138)
 
 #Train entities
-Entities.gmod_subway_81-540.Name = 81 -540 (Nomernoi SPb)
+Entities.gmod_subway_81-540.Name = 81-540 (Nomernoi SPb)
 Entities.gmod_subway_81-717_6.Name = 81-717.6
-Entities.gmod_subway_ezh3ru1.Name = 81 -710 (Ezh3 RU1)
+Entities.gmod_subway_ezh3ru1.Name = 81-710 (Ezh3 RU1)
 Entities.gmod_subway_81-540_2k.Name = 81-540.2К (Nomernoi Kiev)
-Entities.gmod_subway_81-540_8.Name = 81 -540 (Nomernoi SPb)
-Entities.gmod_subway_81-717_freight.Name = 81 -717 (Navlun SPb)
-Entities.gmod_subway_81-722_new.Name = 81 -722 (Yubileyniy)
+Entities.gmod_subway_81-540_8.Name = 81-540 (Nomernoi SPb)
+Entities.gmod_subway_81-717_freight.Name = 81-717 (Navlun SPb)
+Entities.gmod_subway_81-722_new.Name = 81-722 (Yubileyniy)
 Entities.gmod_subway_81-722_3.Name = 81-722.3 (Yubileyniy)
 Entities.gmod_subway_am.Name = Am/Ap tipi vagonlar
 
@@ -44,8 +44,8 @@ MSCP.Header1 = Başlık arka planı
 MSCP.Header2 = Başlık metni
 MSCP.Frame1 = Oyuncu çerçevesi arka planı
 MSCP.Frame2 = Oyuncu çerçeve başlığı
-MSCP.Frame3 = Oynatıcı çerçevesi ön planı
-MSCP.Footer1 = Altbilgi arka planı
+MSCP.Frame3 = Oyuncu çerçevesi ön planı
+MSCP.Footer1 = Alt bilgi arka planı
 MSCP.Footer2 = Alt Menü Metni
 MSCP.Reset = Renkleri varsayılana sıfırla
 
